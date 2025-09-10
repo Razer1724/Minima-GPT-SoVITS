fc170cfd6c8556b336ab79b9d8c69dfcda09b7ae
Razer1724 <184171368+Razer1724@users.noreply.github.com>
2025-09-09 23:20:31 -0500

    Update go-webui.bat

--- go-webui.bat
+++ go-webui.bat
@@ -2,5 +2,6 @@ set "SCRIPT_DIR=%~dp0"
 set "SCRIPT_DIR=%SCRIPT_DIR:~0,-1%"
 cd /d "%SCRIPT_DIR%"
 set "PATH=%SCRIPT_DIR%\runtime;%PATH%"
-runtime\python.exe -I webui.py en_US
+env\python.exe -I webui.py en_US
 pause
+
